e0718faccbc61298fcd35be9c7de9aef9ba48a64
Rennka <miho0_0@naver.com>
2021-07-27 04:17:10 +0900

    DLL Injector 프로젝트 속성 변경 및 오류 수정
    프로젝트 속성 변경 : DebugMT -> ReleaseMT 로 수정,
    작성 시 생긴 오타 및 오류 수정 완료.
    
    빌드 성공 확인하였음. {(victim system-win7,32bit)에서 컴파일 및 실행 테스트 필요함.}
    
    테스트 DLL 파일 (zwCreateFile_Hooker.dll) 작성,  dll_injector에서 exploror.exe로 인젝션 하게끔 작성 완료..

---
+++ Src/DLL_Injector.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
+
+#include <String>
+
+bool process_name_to_pid(
+	__out DWORD& pid,
+	__in const std::wstring& process_name
+);
+
+bool dll_injection(
+	__in DWORD pid,
+	__in const std::wstring& dll_name
+);
+
+int main() {
+	DWORD pid = 0;
+	std::wstring process_name = L"explorer.exe";
+	std::wstring dll_name = L"../../Build/Win32Debug/zwCreateFile_Hooker.dll";
+
+	if (process_name_to_pid(pid, process_name)) {
+		dll_injection(pid, dll_name);
+	}
+	return 0;
+}
+
+bool process_name_to_pid(
+	__out DWORD& pid,
+	__in const std::wstring& process_name
+) {
+	bool result = false;
+	HANDLE snapshot = nullptr;
+	PROCESSENTRY32 entry = {};
+	
+	entry.dwSize = sizeof(PROCESSENTRY32);
+	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+
+	if (snapshot != INVALID_HANDLE_VALUE) {
+		Process32First(snapshot, &entry);
+		do {
+			// 프로세스를 숨겨주는 코드이다. (-> tchar.h)
+			if (!_tcsicmp(process_name.c_str(), 
+				entry.szExeFile)) {
+				pid = entry.th32ProcessID;
+				result = true;
+				break;
+			}
+		} while (Process32Next(snapshot, &entry));
+		CloseHandle(snapshot);
+		}
+	return result;
+}
+
+
+bool dll_injection(
+	__in DWORD pid,
+	__in const std::wstring& dll_name
+) {
+	bool result = false;
+
+	HANDLE process_handle = nullptr;
+	HANDLE thread_handle = nullptr;
+	LPVOID remote_buffer = nullptr;
+	HMODULE module = {};
+
+	LPTHREAD_START_ROUTINE thread_start_routine = nullptr;
+
+	do {
+		process_handle = OpenProcess(PROCESS_ALL_ACCESS,
+			FALSE, pid);
+		if (process_handle == nullptr) {
+			break;
+		}
+
+		remote_buffer = VirtualAllocEx(
+			process_handle,
+			nullptr,
+			dll_name.size(),
+			MEM_COMMIT,
+			PAGE_READWRITE
+		);
+		if (!remote_buffer) {
+			break;
+		}
+		if (!WriteProcessMemory(
+			process_handle,
+			remote_buffer,
+			dll_name.c_str(),
+			dll_name.size() * sizeof(wchar_t),
+			nullptr
+		)) {
+			break;
+		}
+
+		module = GetModuleHandle(L"kernel32.dll");
+		thread_start_routine =
+			(LPTHREAD_START_ROUTINE)GetProcAddress(module,
+				"LoadLibraryW");
+
+		thread_handle = CreateRemoteThread(
+			process_handle,
+			nullptr,
+			0,
+			thread_start_routine,
+			remote_buffer,
+			0,
+			nullptr);
+
+		WaitForSingleObject(thread_handle, INFINITE);
+
+		result = true;
+	} while (false);
+
+	CloseHandle(process_handle);
+	CloseHandle(thread_handle);
+
+	return result;
+}
